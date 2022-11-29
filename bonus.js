91bcde75b61eca6a97f2216085b8db72913963ca
teemo <teemo@DESKTOP-JL25QLR>
2022-11-29 12:20:28 -0300

    firebase up

---
+++ bonus.js
@@ -0,0 +1,135 @@
+
+
+var after = document.createElement('audio');
+var better = document.createElement('audio');
+var doiit = document.createElement('audio');
+var ever = document.createElement('audio');
+var faster = document.createElement('audio');
+var harder = document.createElement('audio');
+var hour = document.createElement('audio');
+var makeIt = document.createElement('audio');
+var makeUs = document.createElement('audio');
+var morethan = document.createElement('audio');
+var never = document.createElement('audio');
+var over = document.createElement('audio');
+var stronger = document.createElement('audio');
+var workit = document.createElement('audio');
+var bonus1 = document.createElement('audio');
+var stage = document.createElement('audio');
+
+after.setAttribute('src', './audio/after.wav');
+better.setAttribute('src', './audio/better.wav');
+doiit.setAttribute('src', './audio/doiit.wav');
+ever.setAttribute('src', './audio/ever.wav');
+faster.setAttribute('src', './audio/faster.wav');
+harder.setAttribute('src', './audio/harder.wav');
+hour.setAttribute('src', './audio/hour1.wav');
+makeIt.setAttribute('src', './audio/makeIt.wav');
+makeUs.setAttribute('src', './audio/makeUs.wav');
+morethan.setAttribute('src', './audio/morethan.wav');
+never.setAttribute('src', './audio/never.wav');
+over.setAttribute('src', './audio/over.wav');
+stronger.setAttribute('src', './audio/stronger.wav');
+workit.setAttribute('src', './audio/workit.wav');
+bonus1.setAttribute('src', './audio/bonus1.wav');
+stage.setAttribute('src', './audio/stage.wav');
+
+
+
+
+$('#after').on("click", function () {
+
+    after.play();
+    after.currentTime = 0;
+});
+
+$('#better').click(function () {
+
+    better.play();
+    better.currentTime = 0
+});
+
+$('#doiit').click(function () {
+
+    doiit.play();
+    doiit.currentTime = 0
+});
+
+$('#ever').click(function () {
+
+    ever.play();
+    ever.currentTime = 0
+});
+
+$('#faster').click(function () {
+
+    faster.play();
+    faster.currentTime = 0
+});
+
+$('#harder').click(function () {
+
+    harder.play();
+    harder.currentTime = 0
+});
+
+$('#hour').click(function () {
+
+    hour.play();
+    hour.currentTime = 0
+});
+
+$('#makeIt').click(function () {
+
+    makeIt.play();
+    makeIt.currentTime = 0
+});
+
+$('#makeUs').click(function () {
+
+    makeUs.play();
+    makeUs.currentTime = 0
+});
+
+$('#morethan').click(function () {
+
+    morethan.play();
+    morethan.currentTime = 0
+});
+
+$('#never').click(function () {
+
+    never.play();
+    never.currentTime = 0
+});
+
+$('#over').click(function () {
+
+    over.play();
+    over.currentTime = 0
+});
+
+$('#stronger').click(function () {
+
+    stronger.play();
+    stronger.currentTime = 0
+});
+
+$('#workit').click(function () {
+
+    workit.play();
+    workit.currentTime = 0
+});
+
+$('#bonus1').click(function () {
+
+    bonus1.play();
+    bonus1.currentTime = 0
+});
+
+$('#stage').click(function () {
+
+    stage.play();
+    stage.currentTime = 0;
+
+})
---
+++ keyboardE.js
@@ -0,0 +1,110 @@
+document.addEventListener('keydown', function (e) {
+  if (e.key == "q") {
+    document.getElementById("after").click();
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "w") {
+    document.getElementById("better").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "e") {
+    document.getElementById("doiit").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "r") {
+    document.getElementById("ever").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "a") {
+    document.getElementById("faster").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "s") {
+    document.getElementById("harder").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "d") {
+    document.getElementById("hour").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "f") {
+    document.getElementById("makeIt").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "z") {
+    document.getElementById("makeUs").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "x") {
+    document.getElementById("morethan").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "c") {
+    document.getElementById("never").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "v") {
+    document.getElementById("over").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "b") {
+    document.getElementById("stronger").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "n") {
+    document.getElementById("workit").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "m") {
+    document.getElementById("bonus1").click();
+
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == "l") {
+    document.getElementById("stage").click();
+
+  }
+});
